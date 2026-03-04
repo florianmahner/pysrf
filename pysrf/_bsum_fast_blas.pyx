2fc3b63ef4470177e2b5480127fb27b04ce0d08a
florianmahner <florian.mahner@icloud.com>
2026-03-04 13:02:40 +0100

    refactor: extract _quartic_root into shared .pxd header
    Eliminates 4-way duplication of the quartic solver across all .pyx files.
    Uses cdef inline for zero-overhead inlining at compile time.

--- pysrf/_bsum_fast_blas.pyx
+++ pysrf/_bsum_fast_blas.pyx
@@ -12,29 +12,13 @@
 
 import numpy as np
 cimport numpy as np
-from libc.math cimport sqrt, cbrt, fabs
+from libc.math cimport fabs
+from pysrf._quartic cimport _quartic_root
 from scipy.linalg.cython_blas cimport dgemv, ddot, daxpy
 
 np.import_array()
 
 ctypedef np.float64_t DTYPE_t
-
-
-cpdef double _quartic_root(double a, double b, double c, double d) nogil:
-    cdef double bb  = b * b
-    cdef double a2  = a * a
-    cdef double p   = (3.0 * a * c - bb) / (3.0 * a2)
-    cdef double q   = (9.0 * a * b * c - 27.0 * a2 * d - 2.0 * b * bb) / (27.0 * a2 * a)
-    cdef double root, delta, tmp
-
-    if c > bb / (3.0 * a):
-        delta = sqrt(q * q * 0.25 + p * p * p / 27.0)
-        root  = cbrt(q * 0.5 - delta) + cbrt(q * 0.5 + delta)
-    else:
-        tmp   = b * bb / (27.0 * a2 * a) - d / a
-        root  = cbrt(tmp)
-
-    return 0.0 if root < 0.0 else root
 
 
 cpdef np.ndarray[DTYPE_t, ndim=2] update_w(double[:, ::1] m,
